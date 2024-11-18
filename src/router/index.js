caaf6cee5f5f0f2fba4d9c5b6a0d8b5446399392
stplk-4k <stplk.4k@gmail.com>
2024-11-18 08:44:17 +0300

    добавление хранилища vuex + некоторые изменеия для vue3, а не для vue2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Home.vue';
 import Catalog from '../pages/Catalog.vue';
 import Service from '../pages/Service.vue';
@@ -8,17 +7,19 @@ import About from '../pages/About.vue';
 import History from '../pages/History.vue';
 import Team from '../pages/Team.vue';
 
-Vue.use(Router);
-
-export default new Router({
-  mode: 'history', 
-  routes: [
+const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/catalog', name: 'Catalog', component: Catalog },
-    { path: '/services', name: 'Service', component: Service },
+    { path: '/service', name: 'Service', component: Service },
     { path: '/contacts', name: 'Contacts', component: Contacts },
     { path: '/about', name: 'About', component: About },
     { path: '/history', name: 'History', component: History },
     { path: '/team', name: 'Team', component: Team },
-  ],
-});
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+  export default router;
