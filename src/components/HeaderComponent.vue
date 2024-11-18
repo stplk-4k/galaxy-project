88b056d32c76e52bcbe5015f1e492264daad13a5
stplk-4k <stplk.4k@gmail.com>
2024-11-18 14:57:27 +0300

    добавление img + добавление logo и стилей в header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,23 @@
 <template>
   <header>
-    <h1>Заголовок</h1>
+    <div id="header">
+        <div class="container">
+            <div class="header">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img class="logo-header p-3" src="../assets/img/logo-blue.png" alt="Логотип">
+                    </div>
+                    <div class="col-sm-5">
+                        <h1>Корпорация «Галактика»</h1>
+                    </div>
+                    <div class="col-sm-4"></div>
+                </div>
+            </div>
+        </div>
+    </div>
   </header>
 </template>
 
 <script setup>
 </script>
+
