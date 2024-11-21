5a6b003181095f9395ea9a62c938f5f6896b6e26
stplk-4k <stplk.4k@gmail.com>
2024-11-21 20:26:06 +0300

    добавление части инфы на catalog page

--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    <h1>Catalog Page</h1>
+  <div class="catalog">
+    <div class="container">
+      <h2>
+        Продукция корпорации «Галактика»
+      </h2>
+      <div class="catalog-content">
+        <div class="row catalog-row">
+          <div class="col-9 catalog-text">
+            <h3>
+              <a href="/pages/products/amm.html">«Галактика AMM»</a>
+            </h3>
+            <p>Постройте прибыльный и эффективный производственный процесс на любом предприятии</p>
+          </div>
+          <div class="col-3">
+            <img class="catalog-img " src="@/assets/img/catalog/amm.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
