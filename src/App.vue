4aa263d78b8bdda9fb680a3ab18760b47ad2f3d0
stplk-4k <stplk.4k@gmail.com>
2024-11-21 13:29:38 +0300

    добавление бокового меню

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,32 @@
   <div id="app">
     <HeaderComponent />
     <MenuComponent />
-    <router-view></router-view>
+
+
+    <div class="container">
+      <div class="main-app">
+        <div class="row align-items-start">
+          <div class="col-sm-3">
+            <SidebarComponent />
+          </div>
+          <div class="col-sm-9">
+            <router-view></router-view>
+          </div>
+        </div>
+        
+
+        
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script setup>
-  import HeaderComponent from './components/HeaderComponent.vue';
-  import MenuComponent from './components/MenuComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MenuComponent from './components/MenuComponent.vue';
+import SidebarComponent from './components/SidebarComponent.vue';
 </script>
 
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <div id="sidebar">
+            <div class="container">
+                <ul class="sidebar-ul">
+                    <li>
+                        <router-link to="/about">О нас</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/history">История</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/team">Сотрудники</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+</script>
