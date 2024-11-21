dd759c7f43fcac306bb07ea1a3162837ae7bd76f
stplk-4k <stplk.4k@gmail.com>
2024-11-21 17:22:25 +0300

    добавление части инфы для home page + решение выравнивания текста и картинки по вертикали

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,28 @@
 <template>
+    <div class="main">
+        <div class="container">
+            <h1>Галактика цифровых решений</h1>
+            <div class="row align-items-start main-row">
+                <div class="col-sm-6 main-text">
+                    <p>
+                        Мы занимаемся разработкой программиного обеспечения уже более 35 лет и входим в число лидеров на
+                        ИТ-рынке
+                    </p>
+                    <button type="button" class="btn btn-primary">Подробнее</button>
+                </div>
+                <div class="col-sm-6">
+                    <a href="/img/bg_main.jpg" target="_blank">
+                        <img class="main-img" src="@/assets/img/bg_main.jpg" >
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
     <div>
-        <h1>Home Page</h1>
+
     </div>
 </template>
 
