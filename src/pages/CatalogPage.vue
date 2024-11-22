623eed20f3196c80cbb160a1ece26c21bf49143f
stplk-4k <stplk.4k@gmail.com>
2024-11-22 11:24:48 +0300

    добавление всех услуг и ссылок на них

--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -5,16 +5,198 @@
         Продукция корпорации «Галактика»
       </h2>
       <div class="catalog-content">
-        <div class="row catalog-row">
-          <div class="col-9 catalog-text">
-            <h3>
-              <router-link to="/product-amm">«Галактика AMM»</router-link>
-              <!-- <a href="/pages/products/amm.html">«Галактика AMM»</a> -->
-            </h3>
-            <p>Постройте прибыльный и эффективный производственный процесс на любом предприятии</p>
+        <!-- 1 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-amm">«Галактика AMM»</router-link>
+              </h3>
+              <p>Постройте прибыльный и эффективный производственный процесс на любом предприятии</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-amm">
+                <img class="catalog-img " src="@/assets/img/catalog/amm.png" alt="«Галактика AMM»">
+              </router-link>
+            </div>
           </div>
-          <div class="col-3">
-            <img class="catalog-img " src="@/assets/img/catalog/amm.png" alt="">
+        </div>
+        <!-- 2 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-cpm">«Галактика CPM»</router-link>
+              </h3>
+              <p>Достигайте совершенства в управлении финансами и принимайте решения в режиме реального времени</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-cpm">
+                <img class="catalog-img " src="@/assets/img/catalog/cpm.png" alt="«Галактика CPM»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 3 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-eam">«Галактика EAM»</router-link>
+              </h3>
+              <p>Снижайте стоимость владения активами и сокращайте затраты на их восстановление</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-eam">
+                <img class="catalog-img " src="@/assets/img/catalog/eam.png" alt="«Галактика EAM»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 4 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-erp">«Галактика ERP»</router-link>
+              </h3>
+              <p>Эффективно решайте текущие и стратегические управленческие задачи современного предприятия</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-erp">
+                <img class="catalog-img " src="@/assets/img/catalog/erp.png" alt="«Галактика ERP»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 5 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-hcm">«Галактика HCM»</router-link>
+              </h3>
+              <p>Эффективно управляйте сотрудниками и повышайте их эффективность</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-hcm">
+                <img class="catalog-img " src="@/assets/img/catalog/hcm.png" alt="«Галактика HCM»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 6 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-mcm">«Галактика MCM»</router-link>
+              </h3>
+              <p>Упрощайте планирование производства связанных между собой предприятий</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-mcm">
+                <img class="catalog-img " src="@/assets/img/catalog/mcm.png" alt="«Галактика MCM»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 7 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-ecm">«Галактика ECM»</router-link>
+              </h3>
+              <p>Управляйте документами и корпоративной информацией</p>
+            </div>
+            <div class="col-3 catalog-img-block">
+              <router-link to="/product-ecm">
+                <img class="catalog-img catalog-img-size" src="@/assets/img/catalog/ecm.png" alt="«Галактика ECM»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 8 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-esb">«Галактика ESB»</router-link>
+              </h3>
+              <p>Получайте все бизнес-данные в едином информационном пространстве</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-esb">
+                <img class="catalog-img " src="@/assets/img/catalog/esb.png" alt="«Галактика ESB»<">
+              </router-link>
+
+            </div>
+          </div>
+        </div>
+        <!-- 9 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-mes">«Галактика MES»</router-link>
+              </h3>
+              <p>Реализуйте планирование по операциям и диспетчеризацию внутри производственных подразделений</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-mes">
+                <img class="catalog-img catalog-img-size" src="@/assets/img/catalog/mes.png" alt="«Галактика MES»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 10 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-vuz">«Галактика ВУЗ»</router-link>
+              </h3>
+              <p>Управляйте учебным процессом и научной деятельностью университета</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-vuz">
+                <img class="catalog-img catalog-img-size" src="@/assets/img/catalog/vuz.png" alt="«Галактика ВУЗ»">
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <!-- 11 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-elms">«Галактика eLMS»</router-link>
+              </h3>
+              <p>Организуйте сквозной процесс обучения с возможностью интеграции с уже существующими системами</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-elms">
+                <img class="catalog-img catalog-img-size" src="@/assets/img/catalog/elms.png" alt="«Галактика eLMS»">
+              </router-link>
+
+            </div>
+          </div>
+        </div>
+        <!-- 12 -->
+        <div class="catalog-row-space">
+          <div class="row catalog-row">
+            <div class="col-9 catalog-text">
+              <h3 class="catalog-h">
+                <router-link to="/product-ruz">«Галактика РУЗ»</router-link>
+              </h3>
+              <p>Эффективно управляйте ресурсами вуза. Экономьте время при подготовке расписания учебных занятий</p>
+            </div>
+            <div class="col-3">
+              <router-link to="/product-ruz">
+                <img class="catalog-img " src="@/assets/img/catalog/ruz.png" alt="«Галактика РУЗ»">
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
