df9b0160a2165676ca9ba593b04f1a37d7b3e570
stplk-4k <stplk.4k@gmail.com>
2024-11-29 08:47:05 +0300

    попытка исправить ошибки при билде

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 3000;
 const uuid = require('uuid');
 const cors = require('cors');
-const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
+// const mysql = require('mysql2/promise');
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const bodyParser = require('body-parser');
 
 app.use(cors({
   origin: 'http://localhost:8081',
@@ -15,48 +15,48 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'users_db',
-});
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'users_db',
+// });
 
-app.post('/api/register', async (req, res) => {
-  const { username, password, email } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const query = 'INSERT INTO users (username, password, email) VALUES (?, ?, ?)';
-  try {
-    const [results] = await db.execute(query, [username, hashedPassword, email]);
-    res.json({ message: 'Пользователь успешно зарегистрирован' });
-  } catch (err) {
-    res.status(400).json({ message: 'Пользователь с таким именем уже существует' });
-  }
-});
+// app.post('/api/register', async (req, res) => {
+//   const { username, password, email } = req.body;
+//   const hashedPassword = await bcrypt.hash(password, 10);
+//   const query = 'INSERT INTO users (username, password, email) VALUES (?, ?, ?)';
+//   try {
+//     const [results] = await db.execute(query, [username, hashedPassword, email]);
+//     res.json({ message: 'Пользователь успешно зарегистрирован' });
+//   } catch (err) {
+//     res.status(400).json({ message: 'Пользователь с таким именем уже существует' });
+//   }
+// });
 
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-  const query = 'SELECT * FROM users WHERE username = ?';
-  try {
-    const [results] = await db.execute(query, [username]);
-    if (results.length === 0) {
-      res.status(401).json({ message: 'Неправильный логин или пароль' });
-    } else {
-      const isValidPassword = await bcrypt.compare(password, results[0].password);
-      if (!isValidPassword) {
-        res.status(401).json({ message: 'Неправильный логин или пароль' });
-      } else {
-        const token = jwt.sign({ userId: results[0].id, username: results[0].username }, 'secretkey', { expiresIn: '1h' });
-        res.json({ token });
-      }
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Ошибка сервера' });
-  }
-});
+// app.post('/api/login', async (req, res) => {
+//   const { username, password } = req.body;
+//   const query = 'SELECT * FROM users WHERE username = ?';
+//   try {
+//     const [results] = await db.execute(query, [username]);
+//     if (results.length === 0) {
+//       res.status(401).json({ message: 'Неправильный логин или пароль' });
+//     } else {
+//       const isValidPassword = await bcrypt.compare(password, results[0].password);
+//       if (!isValidPassword) {
+//         res.status(401).json({ message: 'Неправильный логин или пароль' });
+//       } else {
+//         const token = jwt.sign({ userId: results[0].id, username: results[0].username }, 'secretkey', { expiresIn: '1h' });
+//         res.json({ token });
+//       }
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Ошибка сервера' });
+//   }
+// });
 
 let reviews = [];
 
@@ -67,6 +67,9 @@ app.post('/api/reviews', (req, res) => {
   res.json(newReview);
 });
 
+// app.get('/api/reviews', (res) => {
+//   res.json(reviews);
+// });
 app.get('/api/reviews', (req, res) => {
   res.json(reviews);
 });
