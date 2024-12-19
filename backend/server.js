57fb485eaff8f06c09057b3cd7415649cd0b74f9
stplk-4k <stplk.4k@gmail.com>
2024-12-19 23:56:33 +0300

    отладка

--- backend/server.js
+++ backend/server.js
@@ -95,7 +95,6 @@ app.delete('/api/cart/clear', (req, res) => {
     return res.status(401).json({ message: 'Пользователь не авторизован' });
   }
 
-  // cart = [];
   cart = cart.filter(item => item.username !== username);
   console.log('Cart cleared for user:', username);
 
@@ -214,15 +213,13 @@ app.post('/api/reviews', (req, res) => {
 });
 
 app.delete('/api/reviews/:id', (req, res) => {
-  const { token } = req.body; // Получаем токен из тела запроса
-  const reviewId = req.params.id; // Получаем ID отзыва из параметров
+  const { token } = req.body; 
+  const reviewId = req.params.id; 
 
-  // Проверка токена (можно использовать вашу логику проверки)
   if (!token) {
       return res.status(401).json({ message: 'Пользователь не авторизован' });
   }
 
-  // SQL-запрос для удаления отзыва
   const sql = 'DELETE FROM reviews WHERE id = ?';
   
   feedback_db.run(sql, [reviewId], function(err) {
@@ -238,121 +235,6 @@ app.delete('/api/reviews/:id', (req, res) => {
   });
 });
 
-
-// let reviews = [];
-
-// app.post('/api/reviews', (req, res) => {
-//   const { name, review, gender, email, city } = req.body; 
-
-
-//   if (!name || !review || !city) {
-//       return res.status(400).json({ message: 'Все поля обязательны для заполнения.' });
-//   }
-
-//   const sql = 'INSERT INTO reviews (name, review, gender, city, email, newsletter) VALUES (?, ?, ?, ?, ?, ?)'
-//   feedback_db.run(sql, [name, review, gender, city, email, newsletter], function(err) {
-//         if (err) {
-//             return res.status(500).json({ message: 'Ошибка при сохранении отзыва.' });
-//         }
-//         // Возвращаем новый отзыв с его ID
-//         const newReview = { id: this.lastID, name, review, gender, email, city };
-//         res.status(201).json(newReview);
-//     });
-// });
-
-
-  // const newReview = { id: Date.now(), name, review, gender, email, city }; 
-  // reviews.push(newReview); 
-
-  // res.status(201).json(newReview); 
-// });
-
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
-
-// const services = [
-//   { id: 'product-amm', name: '«Галактика AMM»', description: 'Постройте прибыльный и эффективный производственный процесс на любом предприятии' },
-//   { id: 'product-cpm', name: '«Галактика CPM»', description: 'Достигайте совершенства в управлении финансами и принимайте решения в режиме реального времени' },
-//   { id: 'product-eam', name: '«Галактика EAM»', description: 'Снижайте стоимость владения активами и сокращайте затраты на их восстановление' },
-//   { id: 'product-erp', name: '«Галактика ERP»', description: 'Эффективно решайте текущие и стратегические управленческие задачи современного предприятия' },
-//   { id: 'product-ecm', name: '«Галактика ECM»', description: 'Управляйте документами и корпоративной информацией' },
-//   { id: 'product-esb', name: '«Галактика ESB»', description: 'Получайте все бизнес-данные в едином информационном пространстве' },
-//   { id: 'product-mes', name: '«Галактика MES»', description: 'Реализуйте планирование по операциям и диспетчеризацию внутри производственных подразделений' },
-//   { id: 'product-vuz', name: '«Галактика ВУЗ»', description: 'Управляйте учебным процессом и научной деятельностью университета' },
-//   { id: 'product-elms', name: '«Галактика eLMS»', description: 'Организуйте сквозной процесс обучения с возможностью интеграции с уже существующими системами' },
-//   { id: 'product-ruz', name: '«Галактика РУЗ»', description: 'Эффективно управляйте ресурсами вуза. Экономьте время при подготовке расписания учебных занятий' },
-// ];
-
-// app.post('/api/search-services', (req, res) => {
-//   const searchString = req.body.searchString.toLowerCase();
-//   const filteredServices = services.filter(service => {
-//       return service.name.toLowerCase().includes(searchString) || 
-//              service.description.toLowerCase().includes(searchString);
-//   });
-//   res.json(filteredServices);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
-
-// app.get('/api/products/:id', (req, res) => {
-//   const id = req.params.id;
-//   prod_db.get('SELECT * FROM products WHERE id = ?', [id], (err, row) => {
-//     if (err) {
-//       res.status(500).send(err.message);
-//       return;
-//     }
-//     if (!row) {
-//       res.status(404).send('Продукт не найден');
-//       return;
-//     }
-//     res.json(row);
-//   });
-// });
-
-// const feedback_db = new sqlite3.Database('./feedback.db', (err) => {
-//   if (err) {
-//       console.error(err.message);
-//   }
-//   console.log('Подключено к базе данных SQLite - отзывы');
-// });
-
-// app.get('/api/reviews', (req, res) => {
-//   feedback_db.all('SELECT * FROM reviews', [], (err, rows) => {
-//       if (err) {
-//           res.status(500).json({ error: err.message });
-//           return;
-//       }
-//       res.json(rows);
-//   });
-// });
-
-// app.post('/api/reviews', (req, res) => {
-//   const { name, review, gender, city, email, newsletter } = req.body;
-//   const sql = 'INSERT INTO reviews (name, review, gender, city, email, newsletter) VALUES (?, ?, ?, ?, ?, ?)';
-//   const params = [name, review, gender, city, email, newsletter];
-
-//   feedback_db.run(sql, params, function(err) {
-//       if (err) {
-//           res.status(400).json({ error: err.message });
-//           return;
-//       }
-//       res.status(201).json({ id: this.lastID });
-//   });
-// });
-
-
-// let reviews = [];
-
-// app.post('/api/reviews', (req, res) => {
-//   const { name, review, gender, email, city } = req.body;
-//   const newReview = { id: uuid.v4(), name, review, gender, email, city };
-//   reviews.push(newReview);
-//   res.json(newReview);
-// });
-// app.get('/api/reviews', (req, res) => {
-//   res.json(reviews);
-// });
